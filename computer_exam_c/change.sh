06d7e07ad497e4630d1bdb34a5b4b9f35ecce370
Dumpling <ab60808@gmail.com>
2020-06-26 08:54:44 +0900

    電硬丙伺服器安裝 (mx liunx)

---
+++ computer_exam_c/change.sh
@@ -0,0 +1,19 @@
+if [[ `id -u` -eq 0 ]] ; then
+    echo "Do not run this with sudo (do not run random things with sudo!)." ;
+    exit 1 ;
+fi
+
+sudo cp ~/computer_c_exam/computer_exam_c/cp/vsftpd.conf /etc/vsftpd.conf 
+sudo cp ~/computer_c_exam/computer_exam_c/cp/telnet /etc/xinetd.d/telnet
+sudo cp ~/computer_c_exam/computer_exam_c/cp/.profile /root/.profile
+#sudo cp ~/computer_c_exam/computer_exam_c/cp/lightdm.conf /etc/lightdm/lightdm.conf
+sudo cp ~/computer_c_exam/computer_exam_c/cp/dhcpd.conf /etc/dhcp/dhcpd.conf
+sudo cp ~/computer_c_exam/computer_exam_c/clear.sh ~/Desktop/
+#sudo cp -r ~/computer_c_exam/computer_exam_c/clear-root.sh /root/Desktop/
+sudo chmod 777 ~/Desktop/clear.sh
+sudo service vsftpd restart
+sudo service xinetd restart
+cd ~/computer_c_exam/computer_exam_c/www
+sudo cp -r * /var/www/html
+history -c
+~/computer_c_exam/computer_exam_c/adduser.sh
---
+++ computer_exam_c/install.sh
@@ -0,0 +1,9 @@
+if [[ `id -u` -eq 0 ]] ; then
+    echo "Do not run this with sudo (do not run random things with sudo!)." ;
+    exit 1 ;
+fi
+
+sudo apt update
+sudo apt install apache2 vsftpd openssh-server xinetd telnetd isc-dhcp-server gedit net-tools -y
+sudo chmod 777 ~/computer_c_exam/computer_exam_c/
+~/computer_c_exam/computer_exam_c/change.sh
